214e62318be8a7af33e33778447a2f33db7300f1
Andrew Su <chronicles31098@gmail.com>
2021-11-30 15:13:51 -0800

    Update the repo, in order to update the production environment.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,6 +8,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          I've updated my react code. The concurrent react script will update
+          the local development version of my app. When I push these changes to
+          the repo, AWS Amplify will detect changes to the github repository,
+          and pull the changes. Resulting in changes in the Production version
+          of the app. Luckily, AWS Amplify handles all the web hosting, server
+          setup, and everything.
         </p>
         <a
           className="App-link"
