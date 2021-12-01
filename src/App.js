789042231739ecd5c8fed954c51354d61c0c69ec
Andrew Su <chronicles31098@gmail.com>
2021-12-01 15:08:28 -0800

    Made app.js match the tutorial code.

--- src/App.js
+++ src/App.js
@@ -7,25 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          I've updated my react code. The concurrent react script will update
-          the local development version of my app. When I push these changes to
-          the repo, AWS Amplify will detect changes to the github repository,
-          and pull the changes. Resulting in changes in the Production version
-          of the app. Luckily, AWS Amplify handles all the web hosting, server
-          setup, and everything.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We not have Auth!</h1>
       </header>
       <AmplifySignOut />
     </div>
