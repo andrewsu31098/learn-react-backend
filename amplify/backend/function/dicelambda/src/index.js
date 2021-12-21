186e6369b09e64067b7493977f9f8152fbd30f9c
Andrew Su <chronicles31098@gmail.com>
2021-12-20 20:55:00 -0800

    fixed Lambda Function error.

--- amplify/backend/function/dicelambda/src/index.js
+++ amplify/backend/function/dicelambda/src/index.js
@@ -1,5 +1,7 @@
 exports.handler = async (event) => {
   // TODO implement
+  const min = 1;
+  const max = 6;
   const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
   const response = {
     statusCode: 200,
